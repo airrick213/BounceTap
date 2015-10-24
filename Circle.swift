00e48317f578ebe5da4ab21ac2e176d843034426
airrick213 <erickim213@gmail.com>
2015-10-24 01:34:02 -0400

    added circle to main screen

---
+++ Circle.swift
@@ -0,0 +1,15 @@
+//
+//  Circle.swift
+//  BounceTap
+//
+//  Created by Eric Kim on 10/24/15.
+//  Copyright © 2015 Apportable. All rights reserved.
+//
+
+import Foundation
+
+class Circle: CCSprite {
+    
+    var velocity: Double = 0
+    
+}
