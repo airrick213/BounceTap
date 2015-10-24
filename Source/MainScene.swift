c6d720533bb9fd36fb34a6026143bdcc3516075b
airrick213 <erickim213@gmail.com>
2015-10-24 02:00:42 -0400

    added animated instruction

--- Source/MainScene.swift
+++ Source/MainScene.swift
@@ -1,5 +1,29 @@
 import Foundation
 
-class MainScene: CCNode {
+enum GameState {
+    case Title, Ready, Playing, GameOver
+}
 
+class MainScene: CCNode {
+    
+    weak var background: CCNodeColor!
+    weak var startButton: CCButton!
+    weak var scoreLabel: CCLabelTTF!
+    weak var circle: Circle!
+    weak var tapTheCircle: CCLabelTTF!
+    
+    var gameState: GameState = .Title
+    
+    var score: Int = 0 {
+        didSet {
+            scoreLabel.string = "\(score)"
+        }
+    }
+    
+    func ready() {
+        gameState = .Ready
+        
+        self.animationManager.runAnimationsForSequenceNamed("Ready")
+    }
+    
 }
